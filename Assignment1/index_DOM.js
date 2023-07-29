289c99e9a8c968475896d4e4f0c24e03e01e36c8
ShwetaBharambe5 <shwetabharambe21@gmail.com>
2023-07-30 00:35:44 +0530

    Eighth Commit

--- Assignment1/index_DOM.js
+++ Assignment1/index_DOM.js
@@ -25,6 +25,24 @@ function addItem(e){
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
 
+  var newDescription = document.getElementById('description').value;
+
+  li.appendChild(document.createTextNode(newDescription));
+
+  var editBtn = document.createElement('button');
+
+  // Add classes to edit button
+  editBtn.className = 'btn btn-secondary btn-sm float-right edit';
+
+  // Append text node
+  editBtn.appendChild(document.createTextNode('Edit'));
+
+  // Append button to li
+  li.appendChild(editBtn);
+
+  // Append li to list
+  itemList.appendChild(li);
+
   // Create del button element
   var deleteBtn = document.createElement('button');
 
@@ -39,6 +57,7 @@ function addItem(e){
 
   // Append li to list
   itemList.appendChild(li);
+
 }
 
 // Remove item
@@ -52,20 +71,24 @@ function removeItem(e){
 }
 
 // Filter Items
-function filterItems(e){
-  // convert text to lowercase
-  var text = e.target.value.toLowerCase();
-  // Get lis
-  var items = itemList.getElementsByTagName('li');
-  // Convert to an array
-  Array.from(items).forEach(function(item){
-    var itemName = item.firstChild.textContent;
-    if(itemName.toLowerCase().indexOf(text) != -1){
-      item.style.display = 'block';
-    } else {
-      item.style.display = 'none';
-    }
-  });
+function filterItems(e) {
+    // Convert text to lowercase
+    var text = e.target.value.toLowerCase();
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    // Convert to an array
+    Array.from(items).forEach(function (item) {
+      var itemName = item.firstChild.textContent.trim();
+      var description = item.childNodes[1].textContent.trim(); // Get the text of the second child (description)
+
+      // Check if the search string is found in either the name or description
+      if (itemName.toLowerCase().indexOf(text) !== -1 || description.toLowerCase().indexOf(text) !== -1) {
+        item.style.display = 'block';
+      } else {
+        item.style.display = 'none';
+      }
+    });
+
 }
 
 });
