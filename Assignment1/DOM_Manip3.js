b0e162d8f7c31b654885bc0383e89a1483675df0
ShwetaBharambe5 <shwetabharambe21@gmail.com>
2023-07-29 19:45:12 +0530

    Fifth Commit

--- Assignment1/DOM_Manip3.js
+++ Assignment1/DOM_Manip3.js
@@ -1,27 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const headerTitleElement = document.getElementById('header-title');
-    if (headerTitleElement) {
-      headerTitleElement.textContent = "Hello";
-      headerTitleElement.style.border = "1px solid black";
-    }
+    // const headerTitleElement = document.getElementById('header-title');
+    // if (headerTitleElement) {
+    //   headerTitleElement.textContent = "Hello";
+    //   headerTitleElement.style.border = "1px solid black";
+    // }
 
     const addItemElement = document.querySelector('.title'); 
     if (addItemElement) {
       addItemElement.style.fontWeight = "bold";
-      addItemElement.style.color = "green";
+      //addItemElement.style.color = "green";
     }
 
     var items = document.getElementsByClassName("list-group-item");
-    items[2].style.color = "green"
+    // items[2].style.color = "green"
 
     for(var i=0; i<items.length; i++)
         items[i].style.fontWeight="bold";
 
-    var li_item = document.getElementsByClassName("list-item");
-    li_item[0].style.color="red";
+    // var li_item = document.getElementsByClassName("list-item");
+    // li_item[0].style.color="red";
     
     var li_items = document.getElementsByTagName("li");
     for(var i=0; i<items.length; i++)
         li_items[i].style.border="1px solid black";
+
+    var secondItem = document.querySelector(".list-group-item:nth-child(2)");
+    //secondItem.style.background = "green";
+
+    var thirdItem = document.querySelector(".list-group-item:nth-child(3)");
+    //thirdItem.style.display = "None";
+
+    var LiItems = document.querySelectorAll("#items li");
+    LiItems[1].style.color = "green";
+
+    var oddItems = document.querySelectorAll("#items li:nth-child(odd)");
+    for(var i=0; i<oddItems.length; i++)
+        oddItems[i].style.background = "green";
+
   });
   
