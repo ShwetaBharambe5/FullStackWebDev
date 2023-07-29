5bf5221770d519ff2fa2968966d59d3a697c3e27
ShwetaBharambe5 <shwetabharambe21@gmail.com>
2023-07-29 14:33:58 +0530

    Third Commit

--- Assignment1/DOM_Manip3.js
+++ Assignment1/DOM_Manip3.js
@@ -11,5 +11,11 @@ document.addEventListener("DOMContentLoaded", function () {
       addItemElement.style.color = "green";
     }
 
+    var items = document.getElementsByClassName("list-group-item");
+    items[2].style.color = "green"
+
+    for(var i=0; i<items.length; i++)
+        items[i].style.fontWeight="bold";
+
   });
   
