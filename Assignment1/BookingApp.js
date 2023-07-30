c8f3adda416ea4a3137f8113ae4282bda2e429c7
ShwetaBharambe5 <shwetabharambe21@gmail.com>
2023-07-30 14:26:40 +0530

    Twelveth Commit

--- Assignment1/BookingApp.js
+++ Assignment1/BookingApp.js
@@ -1,5 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
     var form = document.getElementById("UserAppointmentForm");
+    var userList = document.getElementById("userList");
+  
+    // Function to display user data in the list
+    function displayUserData() {
+      userList.innerHTML = "";
+      for (var i = 0; i < localStorage.length; i++) {
+        var key = localStorage.key(i);
+        if (key !== "usersData") {
+          var user = JSON.parse(localStorage.getItem(key));
+          var listItem = document.createElement("li");
+          listItem.innerHTML = `
+            <p>Name: ${user.name}</p>
+            <p>Email: ${user.email}</p>
+            <p>phone: ${user.phone}</p>
+            <button data-email="${user.email}" class="deleteBtn">Delete</button>
+          `;
+          userList.appendChild(listItem);
+        }
+      }
+    }
+  
+    // Function to store user data in local storage
+    function storeUserData(userDetails) {
+      var email = userDetails.email;
+      localStorage.setItem(email, JSON.stringify(userDetails));
+    }
+  
+    // Function to delete user data from local storage
+    function deleteUser(email) {
+      localStorage.removeItem(email);
+    }
+  
+    // Display existing user data on page load
+    displayUserData(); // Call the function to show user data on page load
   
     form.addEventListener("submit", function (e) {
       e.preventDefault();
@@ -16,23 +50,27 @@ document.addEventListener("DOMContentLoaded", function () {
         phone: phone,
       };
   
-      // Get existing user data from local storage
-      var usersData = localStorage.getItem("usersData");
-      var users = usersData ? JSON.parse(usersData) : {}; // Parse existing data or create an empty object
+      // Store the user details in local storage
+      storeUserData(userDetails);
   
-      // Store the user details in the object with email as the key
-      users[email] = userDetails;
-  
-      // Save the updated object back to local storage
-      localStorage.setItem("usersData", JSON.stringify(users));
-  
-      console.log(JSON.parse(localStorage.getItem(userDetails)));
-
       // Clear the form fields after submission (optional)
       form.reset();
   
+      // Display the updated user data in the list immediately after submission
+      displayUserData();
+  
       // Display a success message (optional)
       alert("User details submitted successfully!");
+    });
+  
+    userList.addEventListener("click", function (e) {
+      if (e.target.classList.contains("deleteBtn")) {
+        var email = e.target.dataset.email;
+        // Remove user data from local storage
+        deleteUser(email);
+        // Update the list to reflect the changes
+        displayUserData();
+      }
     });
   });
   
